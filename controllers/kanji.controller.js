c3279b0aabbe8c9c57b19b98b11410d9d811eaa8
clam119 <christopher.lam119@gmail.com>
2022-10-24 19:10:13 +0100

    Add all of the getN5-N1 kanji controller functions.

--- controllers/kanji.controller.js
+++ controllers/kanji.controller.js
@@ -1,10 +1,11 @@
-const { fetchAllKanji, fetchN5Kanji } = require('../models/kanji.model.js');
+const { fetchAllKanji, fetchN5Kanji, fetchN4Kanji, fetchN3Kanji, fetchN2Kanji, fetchN1Kanji } = require('../models/kanji.model.js');
 
 exports.getAllKanji = (req, res, next) => {
     fetchAllKanji()
     .then((kanjiData => {
         res.status(200).send(kanjiData);
     })
+    .catch(next)
 )}
 
 exports.getN5Kanji = (req, res, next) => {
@@ -12,4 +13,39 @@ exports.getN5Kanji = (req, res, next) => {
     .then((n5KanjiData) => {
         res.status(200).send(n5KanjiData);
     })
+    .catch(next)
 }
+
+exports.getN4Kanji = (req, res, next) => {
+    fetchN4Kanji()
+    .then((n4KanjiData) => {
+        res.status(200).send(n4KanjiData);
+    })
+    .catch(next)
+}
+
+exports.getN3Kanji = (req, res, next) => {
+    fetchN3Kanji()
+    .then((n3KanjiData) => {
+        res.status(200).send(n3KanjiData);
+    })
+    .catch(next)
+}
+
+exports.getN2Kanji = (req, res, next) => {
+    fetchN2Kanji()
+    .then((n2KanjiData) => {
+        res.status(200).send(n2KanjiData);
+    })
+    .catch(next)
+}
+
+exports.getN1Kanji = (req, res, next) => {
+    fetchN1Kanji()
+    .then((n1KanjiData) => {
+        res.status(200).send(n1KanjiData);
+    })
+    .catch(next)
+}
+
+
