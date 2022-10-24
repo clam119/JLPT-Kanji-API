c0428223a075099b4bdd2d5bbef93562d76ae9ee
clam119 <christopher.lam119@gmail.com>
2022-10-24 18:43:50 +0100

    Add the API/n5 GET request route.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-const { getAllKanji } = require('./controllers/kanji.controller');
+const { getAllKanji, getN5Kanji } = require('./controllers/kanji.controller');
 
 app.use(express.json());
 
 app.get('/api', getAllKanji);
+
+app.get('/api/n5', getN5Kanji);
 
 app.use((err, req, res, next) => {
     if(!err.status && err.msg) {
