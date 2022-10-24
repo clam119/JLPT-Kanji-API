8c6ae3ff91ababf117b24ce136bf3d967ea4e4d3
clam119 <christopher.lam119@gmail.com>
2022-10-24 19:04:02 +0100

    Add all of the GET request tests to n5, n4, n3, n2, and n1 Kanjis.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -30,16 +30,67 @@ describe('app', () => {
     describe('GET /api/n5', () => {
         it.only('Status: 200 - Sends back an array of all of the JLPT N5 Kanji in the database', async () => {
             const response = await request(app).get('/api/n5').expect(200)
-            const {n5_kanji} = await response;
+            const { _body: n5_kanji } = await response;
             expect(n5_kanji).toHaveLength(5);
-            expect(n5_kanji[0]).toEqual({
-                kanji: "聞",
-                 jlpt: 5,
-                 meanings: "Hear,Ask,Listen",
-                 readings_on: "ぶん,もん",
-                 readings_kun: "き.く,き.こえる"
+            n5_kanji.forEach((kanji) => {
+                expect(kanji).toMatchObject({
+                    jlpt: 5
+                })
             })
         })
     })
+
+    describe('GET /api/n4', () => {
+        it.only('Status: 200 - Sends back an array of all of the JLPT N4 Kanji in the database', async () => {
+            const response = await request(app).get('/api/n4').expect(200)
+            const { _body: n4_kanji } = await response;
+            expect(n4_kanji).toHaveLength(5);
+            n4_kanji.forEach((kanji) => {
+                expect(kanji).toMatchObject({
+                    jlpt: 4
+                })
+            })
+        })
+    })
+
+    describe('GET /api/n3', () => {
+        it.only('Status: 200 - Sends back an array of all of the JLPT N3 Kanji in the database', async () => {
+            const response = await request(app).get('/api/n3').expect(200)
+            const { _body: n3_kanji } = await response;
+            expect(n3_kanji).toHaveLength(5);
+            n3_kanji.forEach((kanji) => {
+                expect(kanji).toMatchObject({
+                    jlpt: 3
+                })
+            })
+        })
+    })
+
+    describe('GET /api/n2', () => {
+        it.only('Status: 200 - Sends back an array of all of the JLPT N2 Kanji in the database', async () => {
+            const response = await request(app).get('/api/n2').expect(200)
+            const { _body: n2_kanji } = await response;
+            expect(n2_kanji).toHaveLength(5);
+            n2_kanji.forEach((kanji) => {
+                expect(kanji).toMatchObject({
+                    jlpt: 2
+                })
+            })
+        })
+    })
+
+    describe('GET /api/n1', () => {
+        it.only('Status: 200 - Sends back an array of all of the JLPT N1 Kanji in the database', async () => {
+            const response = await request(app).get('/api/n1').expect(200)
+            const { _body: n1_kanji } = await response;
+            expect(n1_kanji).toHaveLength(5);
+            n1_kanji.forEach((kanji) => {
+                expect(kanji).toMatchObject({
+                    jlpt: 1
+                })
+            })
+        })
+    })
+
 
 })
