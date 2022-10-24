80da279ee706cb8ee6f29057f35a5d29a268cda5
clam119 <christopher.lam119@gmail.com>
2022-10-24 19:13:01 +0100

    Add all of the get n5-n1 kanji controllers in the import and create all of the routes for /api/n5 - /api/n1

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
-const { getAllKanji, getN5Kanji } = require('./controllers/kanji.controller');
+const { getAllKanji, getN5Kanji, getN4Kanji, getN3Kanji, getN2Kanji, getN1Kanji } = require('./controllers/kanji.controller');
 
 app.use(express.json());
 
 app.get('/api', getAllKanji);
 
 app.get('/api/n5', getN5Kanji);
+app.get('/api/n4', getN4Kanji);
+app.get('/api/n3', getN3Kanji);
+app.get('/api/n2', getN2Kanji);
+app.get('/api/n1', getN1Kanji);
 
 app.use((err, req, res, next) => {
     if(!err.status && err.msg) {
