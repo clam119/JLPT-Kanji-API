815ea496d74797d4aa41d82937fd37870887c622
clam119 <christopher.lam119@gmail.com>
2022-10-24 18:44:11 +0100

    Add the first test for a Status test for api/n5

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -27,4 +27,19 @@ describe('app', () => {
         })
     })
 
+    describe('GET /api/n5', () => {
+        it.only('Status: 200 - Sends back an array of all of the JLPT N5 Kanji in the database', async () => {
+            const response = await request(app).get('/api/n5').expect(200)
+            const {n5_kanji} = await response;
+            expect(n5_kanji).toHaveLength(5);
+            expect(n5_kanji[0]).toEqual({
+                kanji: "聞",
+                 jlpt: 5,
+                 meanings: "Hear,Ask,Listen",
+                 readings_on: "ぶん,もん",
+                 readings_kun: "き.く,き.こえる"
+            })
+        })
+    })
+
 })
