65229be72b4c7893f5c07aece338687cdcd3da02
clam119 <christopher.lam119@gmail.com>
2022-10-24 22:42:45 +0100

    Changed default encoding to be utf-8

--- db/setup.sql
+++ db/setup.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS jlpt_kanji_test;
 DROP DATABASE IF EXISTS jlpt_kanji;
 
-CREATE DATABASE jlpt_kanji_test;
-CREATE DATABASE jlpt_kanji;
+CREATE DATABASE jlpt_kanji_test ENCODING = 'UTF-8';
+CREATE DATABASE jlpt_kanji ENCODING = 'UTF-8';
