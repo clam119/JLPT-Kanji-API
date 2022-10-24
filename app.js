40407eea34afde5fff9da44a3501c49ba62d71b1
clam119 <christopher.lam119@gmail.com>
2022-10-24 20:23:05 +0100

    Changed the endpoint from /api/n5 to /api/jlpt/n5 and for the rest of the JLPT kanjis.

--- app.js
+++ app.js
@@ -8,18 +8,24 @@ app.get('/api', getAllKanji);
 app.get('/api/:kanji_id', getKanjiByID)
 app.get('/api/:kanji_char', getKanjiByChar)
 
-app.get('/api/n5', getN5Kanji);
-app.get('/api/n4', getN4Kanji);
-app.get('/api/n3', getN3Kanji);
-app.get('/api/n2', getN2Kanji);
-app.get('/api/n1', getN1Kanji);
+app.get('/api/jlpt/n5', getN5Kanji);
+app.get('/api/jlpt/n4', getN4Kanji);
+app.get('/api/jlpt/n3', getN3Kanji);
+app.get('/api/jlpt/n2', getN2Kanji);
+app.get('/api/jlpt/n1', getN1Kanji);
 
 app.use((err, req, res, next) => {
-    if(!err.status && err.msg) {
+    if(err.status && err.msg) {
         res.status(err.status).send(err.msg);
     }
     else {
         next(err);
+    }
+})
+
+app.use((err, req, res, next) => {
+    if(err.code='22PO2') {
+        console.log(err)
     }
 })
 app.use((err, req, res, next) => {
